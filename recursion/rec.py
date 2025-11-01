e0d01eb982762b69909431aced9c1f0024a00c43
vinay_a <vinayadari00@gmail.com>
2025-11-01 15:22:03 +0530

    rec

---
+++ recursion/rec.py
@@ -0,0 +1,16 @@
+count = 0
+# def func():
+#   global count # this is mandatory
+#   if count == 4:
+#     return
+#   print("vinay")
+#   count+=1
+#   func() #here recursive call is last hence tail recursion and if the
+
+
+def tailFunc():
+global count
+    if count == 4:
+
+
+func()
